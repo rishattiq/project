4d48ac137b3594068f38b6c18fc484122c3c4127
Sadia.Batool@gmail.com <Sadia.Batool@gmail.com>
2022-10-28 18:01:08 -0700

    backened complete working version

--- frontend/src/Pages/Form.js
+++ frontend/src/Pages/Form.js
@@ -1,95 +1,123 @@
-import React,{useState} from 'react'
-import './Form.css';
-import axios from 'axios';
-
+import React, { useState } from "react";
+import "./Form.css";
 
 export default function Form() {
-  const [Files, setfile] = useState('')
-  const [placename,setPlacename] = useState('')
-  const [Price, setPrice] = useState('')
-  const [Rooms, setRooms] = useState('')
-  let arr=[Rooms];
-  const handleClear = event =>{
-    setRooms(event.target.value);
-  }
- const handleClick = () =>{
-  setRooms('');
- }
- 
-const addPlace = () =>{
-  alert(Price);
-  axios.post('http://localhost:8000/host/addplace',{
-    placename,Price,imageurl:'42873982',Rooms,
-  }).then(function(response){
-    return response;
-  }).catch(function(error){
-    return error;
-  });
-}
+  const [placename, setPlacename] = useState("");
+  const [price, setPrice] = useState("");
+  const [numberofrooms, setNumberofrooms] = useState("");
+  const [imageurl, setImageurl] = useState("");
 
-  const onplacefileHandler=(event)=>{
-    let files= event.target.file;
-    console.log(files);
-    setfile(Files);
-   console.log("fileadded");
-}
+  const addPlace = (e) => {
+    e.preventDefault();
 
-  const onplacenameHandler=(event)=>{
-      setPlacename(event.target.value);
-  }
+    let obj = {
+      placename,
+      numberofrooms,
+      price,
+      imageurl,
+    };
+    
+    fetch("http://localhost:8000/host/addplace", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
 
-  const onpriceHandler=(event)=>{
+      setImageurl('')
+      setNumberofrooms('')
+      setPlacename('')
+      setPrice('')
+  };
 
+  const onplacenameHandler = (event) => {
+    setPlacename(event.target.value);
+  };
+
+  const onimageurlHandler = (event) => {
+    setImageurl(event.target.value);
+  };
+
+  const onpriceHandler = (event) => {
     setPrice(event.target.value);
-  }
+  };
 
-  const onroomHandler=(event)=>{
-   setRooms(event.target.value);
-  }
-  
+  const onnumberofroomsHandler = (event) => {
+    setNumberofrooms(event.target.value);
+  };
 
   return (
-    <> 
-    <div className='container'>
-      <form className='form'>
-      <h1>My Form</h1>
-       
-      <div className='divider'> 
-         <div className='field'>
-              <label>Add Place</label>
-              {
-              }
-              {
-              }
-               <input type="file" onChange={onplacefileHandler} class="btn-warning"/> 
+    <>
+      <div className="container">
+        <form className="form" onSubmit={addPlace}>
+          <h1>My Form</h1>
 
+          <div className="divider">
+            <div className="field">
+              <label>Add Imageurl</label>
+              {}
+              {}
+              <input
+                type="text"
+                onChange={onimageurlHandler}
+                value={imageurl}
+                class="btn-warning"
+              />
+            </div>
+
+            <div>
+              <label className="field1">PlaceName</label>
+              <input
+                type="place"
+                name="place"
+                placeholder="place"
+                value={placename}
+                onChange={onplacenameHandler}
+                className="field1"
+              />
+            </div>
           </div>
 
-          <div >
-           <label className='field1'>PlaceName</label>
-           <input type="place" name="place" placeholder="place" onChange={onplacenameHandler} className='field1'/>
-         </div>
-     </div>
-      
-      <div className='ui-form'>
-        
-         <div className='container' >
-            <label >No of Rooms</label>
-            <input type="No of Rooms" name="Rooms" placeholder="Rooms" onChange={onroomHandler} className='field1'/>
-        
-               <label>Price</label>
-               <input type="Price" name="price" placeholder="price" onChange={onpriceHandler} className='field1'/>
-         </div>
-       </div>
-         
-         <div className='Button'>
-             <button className='ui button transparent' onClick={handleClick}>Clear</button>
-             <button className='ui button blue' onClick={addPlace}>Submit</button>
-             
-         </div>
+          <div className="ui-form">
+            <div className="container">
+              <label>Number of Rooms</label>
+              <input
+                type="No of Rooms"
+                name="Rooms"
+                placeholder="Rooms"
+                onChange={onnumberofroomsHandler}
+                value={numberofrooms}
+                className="field1"
+              />
+
+              <label>Price</label>
+              <input
+                type="Price"
+                name="price"
+                placeholder="price"
+                onChange={onpriceHandler}
+                value={price}
+                className="field1"
+              />
+            </div>
+          </div>
+
+          <div className="Button">
+            {/* <button className='ui button blue' onClick={addPlace}>Submit</button> */}
+            <button className="ui button blue" type="submit">
+              Submit
+            </button>
+          </div>
         </form>
-       </div>
-     </>
-  )
-  
+      </div>
+    </>
+  );
 }
